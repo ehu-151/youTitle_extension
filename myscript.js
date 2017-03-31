802e855fcec02f53c2e9a3a96015b5436fdf940a
ehu <fehuehuf@gmail.com>
2017-03-31 21:20:15 +0900

    First commit

---
+++ myscript.js
@@ -0,0 +1,59 @@
+//通知の設定
+
+//ページリーロード時に通知をする
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    //ページのURLを取得
+    var strUrl = String(tab.url);
+    var thumbnailUrl = getThumbnailUrl(strUrl);
+    //通知の生成
+    //yotubeの音楽が再生されるときに生成
+    if (changeInfo.title !== undefined && strUrl.indexOf("watch?v=") !== -1) {
+        var pageTitle= changeInfo.title;
+        //ページタイトルの余分な部分(末尾の" - Youtube")を削除する
+        pageTitle=pageTitle.substr( 0, pageTitle.length-10);
+        //生成
+        creatNotification(thumbnailUrl,pageTitle);
+    }
+});
+
+
+
+//関数
+
+//通知番号
+function getNotificationId() {
+  var id = Math.floor(Math.random() * 9007199254740992) + 1;
+  return id.toString();
+}
+
+
+//通知の内容
+//表示：タイトル、サムネイル画像
+function creatNotification(thumbnailUrl, pageTitle){
+      chrome.notifications.create(getNotificationId(), {
+          type : 'basic',
+          iconUrl : thumbnailUrl,
+          title : pageTitle,
+          message : 'メッセージ'
+              }
+                                   , function(){})
+      console.log(numAudible);
+}
+
+//URLから動画IDを取得し、サムネイル画像のURLを返す関数
+function getThumbnailUrl(strUrl){
+  //例：https://www.youtube.com/watch?v=  動画ID  &list~~~
+  //動画IDの部分を抜き出す
+  //「&」がないデフォルトのURLの場合分け
+  var lastMovieIdUrl =100;
+  if (strUrl.indexOf("&") !== -1) {
+    lastMovieIdUrl = strUrl.indexOf("&");
+  }
+  //動画IDの切り抜き
+  var movieID = strUrl.substring(32 , lastMovieIdUrl);
+  //サムネイル画像のURLの作成
+  //例：http://i.ytimg.com/vi/動画ID/1.jpg
+  var thumbnailUrl = "http://i.ytimg.com/vi/" + movieID + "/1.jpg";
+
+  return thumbnailUrl;
+}
